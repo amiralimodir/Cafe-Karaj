dc19c1c21b4c65b2e2ff2a4172a367b5c77e9920
Amirali Modir <modirghomiamirali@gmail.com>
2024-06-26 18:28:32 +0330

    Update models.py

--- website/models.py
+++ website/models.py
@@ -9,3 +9,17 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    sugar = models.IntegerField()
+    coffee = models.IntegerField()
+    flour = models.IntegerField()
+    chocolate = models.IntegerField()
+    vertical = models.BinaryField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
