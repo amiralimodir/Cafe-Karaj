e4161201f0c57be3eea2e787ea3e56d00cdf7128
Amirali Modir <modirghomiamirali@gmail.com>
2024-06-26 18:09:41 +0330

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255, primary_key=True)
+    full_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+
+    def __str__(self):
+        return self.username
