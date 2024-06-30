e4b69730d0a01fa1b6fea62abe6d9dc7e594c52c
Saviour <a.h.monji@gmail.com>
2024-06-30 11:31:43 +0330

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import register, user_login, cart_view
+from .views import register, user_login, cart_view, product_list,update_storage_view,add_product_view
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login_view, name='login'),
+    path('login/', user_login, name='login'),
     path('cart/', cart_view, name='cart'),
     path('add_product/', views.add_product_view, name='add_product'),
     path('update_storage/', views.update_storage_view, name='update_storage'),
     path('products/', views.product_list_view, name='product_list')
 
 ]
-
